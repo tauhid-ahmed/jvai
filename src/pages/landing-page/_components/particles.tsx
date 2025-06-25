6ab038521803ba60978dd945788044b30903fc46
Tauhid Ahmed <tauhidahmed.dev@gmail.com>
2025-06-25 22:13:25 +0600

    Add ParticlesComponent for animated background effects on landing page

---
+++ src/pages/landing-page/_components/particles.tsx
@@ -0,0 +1,110 @@
+import { useCallback, useEffect, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import type { Container, ISourceOptions } from "@tsparticles/engine";
+import { loadSlim } from "@tsparticles/slim";
+
+const ParticlesComponent = () => {
+  const [init, setInit] = useState(false);
+
+  // this should be run only once per application lifetime
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+      // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+      // starting from v2 you can add only the features you need reducing the bundle size
+      await loadSlim(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  const particlesLoaded = useCallback(async (container?: Container) => {
+    console.log("Particles container loaded:", container);
+  }, []);
+
+  // You can find more options and customize them here: https://particles.js.org/docs/
+  const options: ISourceOptions = {
+    background: {
+      color: {
+        value: "transparent",
+      },
+    },
+    style: {
+      position: "absolute",
+    },
+    fpsLimit: 120,
+    interactivity: {
+      events: {
+        onClick: {
+          enable: false,
+          mode: "push",
+        },
+        onHover: {
+          enable: false,
+          mode: "repulse",
+        },
+      },
+      modes: {
+        push: {
+          quantity: 5,
+        },
+        repulse: {
+          distance: 100,
+          duration: 0.8,
+        },
+      },
+    },
+    particles: {
+      color: {
+        value: "#f1f1f1",
+      },
+      links: {
+        color: "#ededed",
+        distance: 50,
+        enable: true,
+        opacity: 0.5,
+        width: 1,
+      },
+      move: {
+        direction: "none",
+        enable: true,
+        outModes: {
+          default: "bounce",
+        },
+        random: true,
+        speed: 0.2,
+        straight: false,
+      },
+      number: {
+        density: {
+          enable: true,
+        },
+        value: 600,
+      },
+      opacity: {
+        value: 0.5,
+      },
+      shape: {
+        type: "circle",
+      },
+      size: {
+        value: { min: 1, max: 2 },
+      },
+    },
+    detectRetina: true,
+  };
+
+  if (init) {
+    return (
+      <Particles
+        id="tsparticles"
+        particlesLoaded={particlesLoaded}
+        options={options}
+      />
+    );
+  }
+
+  return <></>;
+};
+
+export default ParticlesComponent;
