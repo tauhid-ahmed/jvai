1902d04190c05aae056c876c5cc47371bc3bc6de
Tauhid Ahmed <tauhidahmed.dev@gmail.com>
2025-06-25 22:12:41 +0600

    Add MenuIcon component for SVG representation of a menu

---
+++ src/components/icons.tsx
@@ -0,0 +1,21 @@
+export function MenuIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-6 w-6"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      width={24}
+      height={24}
+      {...props}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M4 6h16M4 12h16M4 18h16"
+      />
+    </svg>
+  );
+}
