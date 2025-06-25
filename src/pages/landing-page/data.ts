4eeefd50987191640c830bdfe246d1d6b3026664
Tauhid Ahmed <tauhidahmed.dev@gmail.com>
2025-06-25 22:13:10 +0600

    Add sections and navigation data for landing page

---
+++ src/pages/landing-page/data.ts
@@ -0,0 +1,32 @@
+export const sections = {
+  about: "#about",
+  ourSolutions: "#our-solutions",
+  inAction: "#in-action",
+  technology: "#technology",
+  benefits: "#benefits",
+  caseUse: "#case-use",
+};
+
+export const navigationData = [
+  {
+    name: "About",
+    section: sections.about,
+  },
+  {
+    name: "Our Solutions",
+    section: sections.ourSolutions,
+  },
+  {
+    name: "In Action",
+    section: sections.inAction,
+  },
+  {
+    name: "Technology",
+    section: sections.technology,
+  },
+  {
+    name: "Benefits",
+    section: sections.benefits,
+  },
+  { name: "Case Use", section: sections.caseUse },
+];
