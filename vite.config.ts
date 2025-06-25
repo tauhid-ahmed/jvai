c13995e972b798ff913adede8a9d7de9ba3336ee
Tauhid Ahmed <tauhidahmed.dev@gmail.com>
2025-06-25 17:54:20 +0600

    Add webfont download plugin to Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import webfontDownload from "vite-plugin-webfont-dl";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    webfontDownload([
+      'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"',
+    ]),
+  ],
 });
